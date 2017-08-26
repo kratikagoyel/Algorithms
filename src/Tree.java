bf4d2230d52714784706b848a3add855d8b5afb6
kratikagoyel <kratikagoyel@gmail.com>
2017-08-27 00:00:16 +0530

    Basic Data Structure commited Implemented Tree's basic features

--- src/Tree.java
+++ src/Tree.java
@@ -4,6 +4,7 @@
 public class Tree<T> {
     int count=0;
     boolean isIdentical = true;
+    static TreeNode<Integer> root;
     //Count the leaf nodes in a tree
     int countLeaf(TreeNode root){
         if(root != null){
@@ -17,6 +18,66 @@ public class Tree<T> {
         return count;
     }
 
+    static void createTree(int element){
+        if(root == null){
+            root = new TreeNode<>();
+         root.data = element;
+         root.right = null;
+         root.left = null;
+        }
+    }
+
+    static void insetElement(int element){
+        TreeNode<Integer> temp= root;
+        if(root!=null){
+            while (temp!=null) {
+                if (element <= temp.data && (temp.left == null)) {
+                    temp.left = createNode(element);
+                    break;
+                }else if((element > temp.data) && (temp.right == null)){
+                    temp.right = createNode(element);
+                    break;
+                }else if(element <= temp.data && (temp.left != null)){
+                    temp= temp.left;
+                }else if((element > temp.data) && (temp.right != null)){
+                    temp= temp.right;
+                }
+            }
+        }else{
+            createTree(element);
+        }
+    }
+    static TreeNode<Integer> createNode(int element){
+        TreeNode<Integer> temp1 = new TreeNode<>();
+        temp1.data = element;
+        temp1.left = null;
+        temp1.right = null;
+        return temp1;
+    }
+
+    static void print(TreeNode<Integer> root){
+        //Inorder Traversal
+        if(root.left!=null)
+            print(root.left);
+        System.out.println(root.data);
+        if(root.right!=null)
+            print(root.right);
+    }
+    static boolean findElement(int element){
+        TreeNode<Integer> temp = root;
+        boolean flag = false;
+        while(temp!=null) {
+            if (element == temp.data) {
+                flag = true;
+                break;
+            } else if (element < temp.data) {
+                temp = temp.left;
+            } else {
+                temp = temp.right;
+            }
+        }
+        return flag;
+    }
     //Two trees are identical or not
     boolean isIdentical(TreeNode root1, TreeNode root2){
         if(root1 == null && root2 == null)
